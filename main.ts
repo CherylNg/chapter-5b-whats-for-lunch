53293a618e4e21cff6492823d45223e683a15d5b
CherylNg <61680146+CherylNg@users.noreply.github.com>
2020-05-21 09:24:06 +0800

    Change function names
    Change function names to accommodate current MakeCode release

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ basic.forever(function () {
         basic.showIcon(IconNames.Heart)
         basic.showIcon(IconNames.SmallHeart)
     }
-    music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
-    Choice = randint(0, Lunch_options.length - 1)
+    music.beginMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+    Choice = Math.randomRange(0, Lunch_options.length - 1)
     basic.showString("" + (Lunch_options[Choice]))
     while (!(edubitIrBit.isIrSensorTriggered())) {
     	
